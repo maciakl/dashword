4c103691b24d08674023456eb15d623183806c77
Luke Maciak <luke@maciak.net>
2023-08-20 20:00:30 -0400

    First commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use clap::Parser;
+use rand::Rng;
+
+
+/// Generates a dashword (a word followed by a dash and a number) of arbitrary length.
+/// By default it generates an 8 character dasshword composed of a 5 letter word, followed by a
+/// dash and 2 digit number.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Length of the word
+    #[arg(short, long, default_value_t = 5)]
+    length: usize,
+
+    /// Number of digits after the dash
+    #[arg(short, long, default_value_t = 2)]
+    digits: usize,
+}
+
+fn main() {
+
+    let args = Args::parse();
+
+    let len = args.length;
+    let digits = args.digits;
+
+
+    let mut rng = rand::thread_rng();
+
+    let mut number = String::from("");
+
+    for _ in 0..digits {
+       let r = rng.gen_range(0..9); 
+       number.push_str(&r.to_string());
+    }
+
+    let word = random_word::gen_len(len, random_word::Lang::En).unwrap();
+
+
+
+    println!("{}-{}", word, number);
+}
